336213674e49fb55bcbfa1c149aa2b14a3c12e24
jaddap2 <jaddap2@nih.gov>
2021-07-29 16:48:55 -0400

    initial commit

---
+++ apps/react-cookie-service-example/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'react-cookie-service-example',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/react-cookie-service-example',
+};
