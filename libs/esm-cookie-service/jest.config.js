52d775efaa7cff94c2967bcef20c474ab078f22a
Pavan Jadda <17564080+pavankjadda@users.noreply.github.com>
2022-03-18 15:03:01 -0400

    feat(esm-cookie-service): creates `esm-cookie-service` library and published it to NPM

---
+++ libs/esm-cookie-service/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  displayName: 'esm-cookie-service',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/esm-cookie-service',
+};
---
+++ libs/esm-cookie-service/src/index.ts
@@ -0,0 +1,1 @@
+export { check, getCookie, getAllCookies, setCookie, deleteCookie, deleteAllCookies } from './lib/esm-cookie-service';
