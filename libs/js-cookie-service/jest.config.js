c079493779c69a865ca72aef2d2b37c97f0a7d90
Pavan Jadda <17564080+pavankjadda@users.noreply.github.com>
2022-05-15 16:28:49 -0400

    chore(deps): updates NX to 14.1.5

--- libs/js-cookie-service/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  displayName: 'js-cookie-service',
-  preset: '../../jest.preset.js',
-  transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/libs/js-cookie-service',
-};
---
+++ libs/js-cookie-service/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: "js-cookie-service",
+  preset: "../../jest.preset.js",
+  transform: {
+    "^.+\\.[tj]sx?$": "babel-jest"
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  coverageDirectory: "../../coverage/libs/js-cookie-service"
+};
