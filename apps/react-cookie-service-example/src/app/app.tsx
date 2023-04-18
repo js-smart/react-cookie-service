d40090a2d493ce2b89e38746e5a6b4d0cedb3b2c
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2023-04-18 13:29:48 -0400

    feat(core): upgrades NX to 15.9.2 (#22)
    * feat(core): upgrades NX to 15.x.x
    
    * feat(core): upgrades NX to 15.x.x
    
    * feat(core): upgrades NX to 15.x.x

--- apps/react-cookie-service-example/src/app/app.tsx
+++
@@ -1,8 +0,0 @@
-import useCookies from '@js-smart/react-cookie-service';
-
-export function App() {
-  const { getAllCookies } = useCookies();
-  return <div>{JSON.stringify(getAllCookies())}</div>;
-}
-
-export default App;
