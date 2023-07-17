11d255d4fd5ed80d73e4e7bf1d73f89bfabc0fdb
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2023-07-17 09:14:03 -0400

    feat(core): upgrades to NX 16.5.x and other deps

--- libs/js-cookie-service/jest.config.ts
+++ libs/js-cookie-service/jest.config.ts
@@ -1,10 +1,10 @@
 /* eslint-disable */
 export default {
-  displayName: "js-cookie-service",
-  preset: "../../jest.preset.js",
-  transform: {
-    "^.+\\.[tj]sx?$": ['babel-jest', { presets: ['@nrwl/react/babel'] }]
-  },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-  coverageDirectory: "../../coverage/libs/js-cookie-service"
+	displayName: 'js-cookie-service',
+	preset: '../../jest.preset.js',
+	transform: {
+		'^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+	},
+	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+	coverageDirectory: '../../coverage/libs/js-cookie-service',
 };
--- libs/react-cookie-service/jest.config.ts
+++ libs/react-cookie-service/jest.config.ts
@@ -1,10 +1,10 @@
 /* eslint-disable */
 export default {
-  displayName: "react-cookie-service",
-  preset: "../../jest.preset.js",
-  transform: {
-    "^.+\\.[tj]sx?$": ['babel-jest', { presets: ['@nrwl/react/babel'] }]
-  },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-  coverageDirectory: "../../coverage/libs/react-cookie-service"
+	displayName: 'react-cookie-service',
+	preset: '../../jest.preset.js',
+	transform: {
+		'^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+	},
+	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+	coverageDirectory: '../../coverage/libs/react-cookie-service',
 };
